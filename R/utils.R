eea70fef278797ae9e945e0a67e14dc2662dfdb6
kimjam <jamesykim10@gmail.com>
2015-08-04 17:38:21 -0400

    check if queried table needs to be filtered by date

--- R/utils.R
+++ R/utils.R
@@ -8,51 +8,54 @@
 #' @return results of query in a dataframe
 
 nfl_query <- function(query, target_date = '2002-09-05') {
-    
-    db <- RMySQL::dbConnect(MySQL(), 
+
+    db <- RMySQL::dbConnect(MySQL(),
                             user='root',
                             password='Datmysqljawn97%!',
                             dbname='nfl',
                             host='localhost'
     )
-    
+
     rs <- RMySQL::dbSendQuery(conn = db,
                               statement = query
     )
-    
+
     data <- RMySQL::fetch(rs, n = -1)
-    data$date <- as.Date(data$date)
-    
-    data <- data %>%
-        dplyr::filter(date >= as.Date(target_date))
-    
+
+    query_split = unlist(strsplit(query, ' '))
+    pos_tables = c('qb', 'rb', 'wr', 'te')
+    if (query_split[match('from', query_split) + 1] %in% pos_tables) {
+        data <- data %>%
+            dplyr::filter(date >= target_date)
+    }
+
     RMySQL::dbClearResult(rs)
-    
+
     DBI::dbDisconnect(db)
-    
+
     return(data)
 }
 
 #' @title nfl_insert
 #' @description \code{nfl_insert} a utility function to insert data into nfl schema
-#' 
+#'
 #' @param dataframe a dataframe that contains the data to be inserted into table
 #' @param table the name of the table to insert the dataframe into
 
 nfl_insert <- function(dataframe, table) {
-    db <- RMySQL::dbConnect(MySQL(), 
+    db <- RMySQL::dbConnect(MySQL(),
                             user='root',
                             password='Datmysqljawn97%!',
                             dbname='nfl',
                             host='localhost'
     )
-    
-    RMySQL::dbWriteTable(conn = db, 
-                         name = table, 
-                         value = dataframe, 
-                         append = TRUE, 
+
+    RMySQL::dbWriteTable(conn = db,
+                         name = table,
+                         value = dataframe,
+                         append = TRUE,
                          row.names=FALSE
     )
-    
+
     DBI::dbDisconnect(db)
-} 
+}
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # dfstoolkit
 Personal R package to load and clean fantasy sports data to make projections for daily fantasy sports.
 
-Database is populated using python web scrapers. Data from www.pro-football-reference.com
+Database is populated using python [web scrapers](www.github.com/kimjam/pfr_scraper). Data from www.pro-football-reference.com
