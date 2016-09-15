b0485226dfeabf7bcf6a3588babc233e29bfd921
kimjam <jamesykim10@gmail.com>
2016-09-15 19:26:14 -0400

    add create_profiles and docs

---
+++ R/create_profiles.R
@@ -0,0 +1,43 @@
+#' @title create_profiles
+#' @description Function to create profiles for players.
+#'
+#' @param df Dataframe to create profiles from.
+#' @param window_size Size of rolling window. Defaults to 3.
+#'
+#' @return Returns nested list of lists containing profile and projection.
+#' @export
+create_profiles <- function(df, window_size = 3) {
+
+    df %<>%
+        dplyr::arrange(date) %>%
+        dplyr::mutate(row_index = 1:nrow(.))
+
+    df_long <- df %>%
+        dplyr::select(-date, -week, -dk_pts) %>%
+        tidyr::gather(stat, value, c(cmp:fmbl, snaps, snap_pct))
+
+    if (max(df$row_index) <= window_size) {
+        return(NULL)
+    } else {
+        starts <- 1:(max(df$row_index) - window_size)
+        ends <- starts + window_size - 1
+        projections <- ends + 1
+
+        profs <- purrr::map2(
+            .x = starts,
+            .y = ends,
+            .f = ~ df_long %>%
+                dplyr::filter(row_index %in% .x:.y) %>%
+                dplyr::arrange(stat, row_index)
+        )
+
+        projections <- df$dk_pts[projections]
+
+        profiles <- purrr::map2(
+            .x = profs,
+            .y = projections,
+            .f = ~list(profile = .x, projection = .y)
+        )
+        return(profiles)
+    }
+}
